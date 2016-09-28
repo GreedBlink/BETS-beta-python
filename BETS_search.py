9eb82598a550b3bad8dc32d93d823856f4db700a
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2016-09-28 15:00:23 -0300

    translation start of bets_search function

--- BETS_search.py
+++ BETS_search.py
@@ -1,5 +1,25 @@
-print("test")
+bacen_final_ptv1 = 10
+bacen_v7 = 5
 
-a = 2
+def bets_search(start, description = None, src = None, periodicity = None, unit = None, code = None, view=True, lang="en"):
+    if lang == "pt":
+        database = base_final_ptv1
 
-print(aaa)
+    else:
+        database = bacen_v7
+
+    if description == None and src == None and periodicity == None and unit == None and code == None:
+        print("No search parameters. Please set the values of one or more parameters.")
+
+    params = []
+
+    if description != None:
+        ## Break description parameters
+        and_params = []
+        or_params = []
+
+        # Workaround
+        description = ''.join([description])
+
+
+bets_search(start=10)
