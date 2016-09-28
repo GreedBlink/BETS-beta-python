ede0c043395900510a37dc8913d246109680d25f
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2016-09-28 18:20:46 -0300

    continuing the work.

--- BETS_search.py
+++ BETS_search.py
@@ -1,7 +1,10 @@
+import re as re
+
 bacen_final_ptv1 = 10
 bacen_v7 = 5
 
-def bets_search(start, description = None, src = None, periodicity = None, unit = None, code = None, view=True, lang="en"):
+def bets_search(start, description=None, src=None, periodicity=None, unit=None, code=None, view=True, lang="en"):
+
     if lang == "pt":
         database = base_final_ptv1
 
@@ -14,12 +17,39 @@ def bets_search(start, description = None, src = None, periodicity = None, unit 
     params = []
 
     if description != None:
-        ## Break description parameters
+        # Break description parameters
+
         and_params = []
         or_params = []
 
         # Workaround
-        description = ''.join([description])
+        description = ' '.join([description])
+
+        # TODO regex
+        exprs = ["regex", "regex2"]
+
+        if len(exprs) != 0:
+
+            for i in range(len(exprs)):
+                description = "string1"
+                exprs[i] = "string2"
+                exprs[i] = "string3"
+                exprs[i] = "  string4  ".strip()
+                and_params.append(''.join(["Description not like", "\'%", exprs[i], "%\'"]))
 
 
-bets_search(start=10)
+    # Match whole expressions
+    exprs = "regex3"
+
+    if len(exprs) != 0:
+        for i in range(len(exprs)):
+            description = re.sub(exprs[i], "", description)
+            exprs[i] = re.sub("'", "", exprs[i])
+            exprs[i] = exprs[i].strip()
+            or_params.append(''.join(["Description like", "\'%", exprs[i], "%\'"]))
+
+    # Do not match words
+    words = description.str.extract("~ ?(.*?) ", expand=True)[0]
+
+
+bets_search(start=10, description="teste")
