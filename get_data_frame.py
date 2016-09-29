f3d74c139bd533e19351def833a5698fc3f8d1cc
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2016-09-29 13:05:03 -0300

    get_data_frame function created, still working on it.

--- get_data_frame.py
+++ get_data_frame.py
@@ -1,6 +1,37 @@
-
-
+from stemming.porter2 import stem
+import sql as sql
+from sql.aggregate import *
+from sql.conditionals import *
 
 
 def get_data_frame(code, ts=None):
+
+    abbr = "values"
+
+    if ts == None:
+
+        result = bets_search(code=code, view=False)
+
+        if len(result) != 0:
+            abbr = [stem(word) for word in result[0,1]]
+            freq = str(result[0, 2].strip())
+
+        if freq == "A":
+            database = "ts_anuais"
+        elif freq == "Q":
+            database = "ts_trimestrais"
+        elif freq == "M":
+            database = "ts_mensais"
+        elif freq == "W":
+            database = "ts_semanais"
+        elif freq == "D":
+            database = "ts_diarias"
+
+        query = "select data, value from " + database + " where serie like " + "\'" + code + "\'"
+        series = sql.Query(query)
+
+        if len(result) == 0:
+            # TODO implement msg() function
+            return msg(.MSG_NOT_AVAILABLE)
+
 
