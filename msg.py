fc8c5ab0fb5a160cdfeabf0baddba2f636d3dc87
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2016-10-05 15:44:30 -0300

    fixing msg function.

--- msg.py
+++ msg.py
@@ -4,10 +4,10 @@ def msg(*args, skip_before=True, skip_after=False):
     for thing in enumerate(args):
         m = "BETS-package:" + args
 
-    if skip_before:
-        k = "\n" + m
+        if skip_before:
+            k = "\n" + m
 
-    if skip_after:
-        k = m + "\n"
+        if skip_after:
+            k = m + "\n"
 
-    print(k)
+        print(k)
